f2cc50a1c0e73c69824761ea8f921ece6d1ddcac
Safwan Islam <saislam@davidson.edu>
2022-06-22 15:13:54 -0500

    was able to save and click back to display saved things

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const notes = require('./db/db.json');
-// const { uuid } = require('./public/utils/id');
+const { uuid } = require('./public/utils/id');
 
 const app = express();
 app.use(express.static('./public'));
-
-
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => 
-res.sendFile(path.join(__dirname, './public/index.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, './public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-
+app.get('api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'))
+});
 
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request received to get notes`);
@@ -30,30 +28,31 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-    let response;
-    
-    if (req.body && req.body.title && req.body.text) {
-      response = {
-        status: 'success',
-        title: req.body,
-      };
-        notes.push({
-        ...req.body,
-        // note_id = uuid()
-      });
-      res.json(`Note for ${response.data.product} has been added!`);
-    } else {
-      res.json('Request body must at least contain a product name');
-    }
-  
-    console.log(req.body);
-  });
-  
-  // app.delete('/api/notes/:id', (req, res) => {
-  //   res.json(notes);
-  // });
+  console.info(`${req.method} request received to add a note`);
 
-  app.listen(PORT, () =>
-    console.log(`Express server listening on port http://localhost:${PORT}`)
-  );
+  if (req.body && req.body.title && req.body.text) {
+    const pointer = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid(),
+    };
+    notes.push(pointer);
+    let answers = JSON.stringify((notes), null, 2);
+    fs.writeFile('./db/db.json', answers, () => {
+      const input = {
+        body: savedNote,
+      }
+      res.json(input);
+    })
+  };;
+});
+
+// app.delete('/api/notes/:id', (req, res) => {
+//   res.json(notes);
+// });
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port http://localhost:${PORT}`)
+);
+
+
